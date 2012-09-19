2dcb7109b0ae3e4a0c80ff5fbd6484bf3dca494b
David Scott <oghmao@Davids-MacBook-Pro.local>
2012-09-19 11:41:31 -0500

    Initial Commit

---
+++ UMKCFeildGuide/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  UMKCFeildGuide
+//
+//  Created by David Scott on 9/19/12.
+//  Copyright (c) 2012 David Scott. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
